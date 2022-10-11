5b548b48deb7bc93231690803422874330a3f0c6
Rafael Benitez Duarte <rafaelbenitezdaurtesharp@gmail.com>
2022-10-11 16:23:53 -0400

    fist commit

---
+++ Filmes/Controllers/PessoasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Filmes.Models;
+
+namespace Filmes.Controllers
+{
+    public class PessoasController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Pessoas
+        public ActionResult Index()
+        {
+            return View(db.pessoa.ToList());
+        }
+
+        // GET: Pessoas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoa pessoa = db.pessoa.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pessoas/Create
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Codigo,Nome,Email")] Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.pessoa.Add(pessoa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pessoa);
+        }
+
+        // GET: Pessoas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoa pessoa = db.pessoa.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // POST: Pessoas/Edit/5
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Codigo,Nome,Email")] Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pessoa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoa pessoa = db.pessoa.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // POST: Pessoas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pessoa pessoa = db.pessoa.Find(id);
+            db.pessoa.Remove(pessoa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Filmes/Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Filmes.Models
+{
+    public class Contexto : DbContext
+    { 
+        public DbSet<Pessoa> pessoa { get; set; }
+
+    }
+}
